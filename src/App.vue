1fd0249fc8d7a4a8f76556ac5ff42f997f878e38
gabsgc <gabriellagarccia@gmail.com>
2021-10-01 18:33:25 -0300

    create AppTopBar component

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,19 @@
 <template>
   <v-app>
-      <v-app-bar app color="green" dark>
-      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
-      <v-toolbar-title>
-        Campeonato Brasileiro ⚽
-      </v-toolbar-title>
-    </v-app-bar>
-    <v-navigation-drawer
-      v-model="drawer"
-      absolute
-      temporary
-    >
-      <v-list nav dense>
-        <v-list-item-group
-          v-model="group"
-          active-class="green--text text--lighten-2"
-        >
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Início</v-list-item-title>
-          </v-list-item>
-
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-soccer</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Classificação</v-list-item-title>
-          </v-list-item>
-
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-main>
+      <AppTopBar/>
       <Inicio/>
-    </v-main>
   </v-app>
 </template>
 
 <script>
 import Inicio from './components/Inicio'
+import AppTopBar from './components/shared/AppTopBar'
 
 export default {
   name: 'App',
   components: {
-    Inicio
-  },
-  data: () => ({
-    drawer: false,
-    group: null,
-  }),
+    Inicio,
+    AppTopBar
+  }
 };
 </script>
---
+++ src/components/shared/AppTopBar.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="app-top-bar">
+        <v-app-bar app color="green" dark>
+            <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+            <v-toolbar-title>
+                Campeonato Brasileiro ⚽
+            </v-toolbar-title>
+        </v-app-bar>
+        <v-navigation-drawer v-model="drawer" absolute temporary>
+        <v-list nav dense>
+            <v-list-item-group v-model="group" active-class="green--text text--lighten-2">
+            <v-list-item>
+                <v-list-item-icon>
+                <v-icon>mdi-home</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>Início</v-list-item-title>
+            </v-list-item>
+
+            <v-list-item>
+                <v-list-item-icon>
+                <v-icon>mdi-soccer</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>Classificação</v-list-item-title>
+            </v-list-item>
+
+            </v-list-item-group>
+        </v-list>
+        </v-navigation-drawer>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AppTopBar',
+    data() {
+        return {
+            drawer: false,
+            group: null,
+        }
+    },
+}
+</script>
