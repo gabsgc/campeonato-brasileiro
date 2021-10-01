ed32a2f37f8ab461842345888a78f4d8ea8bf64f
gabsgc <gabriellagarccia@gmail.com>
2021-09-30 21:31:29 -0300

    initial settings and create Inicio component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,56 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+      <v-app-bar app color="green" dark>
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+      <v-toolbar-title>
+        Campeonato Brasileiro ⚽
+      </v-toolbar-title>
+    </v-app-bar>
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list nav dense>
+        <v-list-item-group
+          v-model="group"
+          active-class="green--text text--lighten-2"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Início</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-soccer</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Classificação</v-list-item-title>
+          </v-list-item>
+
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <Inicio/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Inicio from './components/Inicio'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    Inicio
+  },
+  data: () => ({
+    drawer: false,
+    group: null,
+  }),
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Inicio.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-container>
+        <h2 class="text-h5 text-center mb-3 mt-5">Brasileirão Série A</h2>
+        <v-img 
+          class="inicio-imagem mb-2"
+          src="https://diariodonordeste.verdesmares.com.br/image/contentid/policy:1.2974821:1596806515/Serie-A.png"
+        >
+        </v-img>
+        <p>
+            A Série A do Campeonato Brasileiro de Futebol de 2020, oficialmente Brasileirão Assaí – 
+            Série A 2020 por motivos de patrocínio, é a 64.ª edição da principal divisão do futebol 
+            brasileiro. A disputa tem o mesmo regulamento dos anos anteriores, quando foi implementado o 
+            sistema de pontos corridos.
+        </p>
+    </v-container>
+</template>
+
+<script>
+export default {
+   name: 'Inicio'
+}
+</script>
+
+<style scoped>
+.inicio-imagem {
+    border-radius: 3px;
+}
+</style>
